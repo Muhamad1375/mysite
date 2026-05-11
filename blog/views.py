fdfa42b433ddd2fb3ed303069061e3f0df508916
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2026-05-11 15:40:40 +0330

    changing elements dynamic in blog

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    context = {'title': 'bitcoin crashed again', 'content': 'bitxoin was flying but now grounded as always', 'author': 'MoAli'}
+    return render(request, 'blog/blog-single.html', context)

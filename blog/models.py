def0ba77717d5963cd8a890442dd131c5c3e93c1
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2026-05-14 14:29:31 +0330

    two tables added contact and blogs post. continue modulations

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    #image
     title = models.CharField(max_length=255)
     content = models.TextField()
+    #tags
+    #category
+    #author
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    
